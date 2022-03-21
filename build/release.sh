c8f734a235632d30d5d6a2d7a712e9125a8ca46f
梁照彬 <liangzhaobin@lucidada.cn>
2022-03-21 09:28:10 +0800

    测试发布

--- build/release.sh
+++ build/release.sh
@@ -36,11 +36,11 @@ then
   npm version $VERSION --message "[release] $VERSION"
 
   # publish
-  git push eleme master
-  git push eleme refs/tags/v$VERSION
+  git push master
+  git push refs/tags/v$VERSION
   git checkout dev
   git rebase master
-  git push eleme dev
+  git push dev
 
   if [[ $VERSION =~ "beta" ]]
   then
